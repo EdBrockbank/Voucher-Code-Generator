f55701ea6d926b8155227de27a554320c8729529
Ed Brockbank <48080384+EdBrockbank@users.noreply.github.com>
2021-03-26 08:42:37 +0000

    Add files via upload

---
+++ codeCalc.js
@@ -0,0 +1,50 @@
+let codes = [];
+let codeDesc = [];
+let ticketRes = [];
+
+function collect(){
+    let codeNum = $("#formdiv").children().length;
+    console.log(codeNum);
+    for (let i = 0; i < codeNum; i++){
+        let intRef = $('#' + i + 'IRef').val();
+        let name = $('#' + i + 'name').val();
+        let hRef = $('#' + i + 'HRef').val();
+        let type = $('#' + i + 'select').val();
+        let number = $('#' + i + 'Num').val();
+        // console.log(intRef + " , " + name + " , " + hRef + " , " + type + " , " + number);
+        calc(intRef, name, hRef, type, number);
+    }
+    display();
+}
+
+function calc(intRef, name, hRef, type, number){
+    console.log(intRef + " , " + name + " , " + hRef + " , " + type + " , " + number);
+    let shortRef = intRef.substr(intRef.length - 4);
+    let nameSplit = name.split(" ");
+    let initials = nameSplit[0].charAt(0) + nameSplit[1].charAt(0);
+    initials = initials.toUpperCase();
+    if (type === "10"){
+        let code = initials + shortRef + "TEN" + number;
+        let desc = name + " £10 off £25 | Int Ref: " + intRef + " - HD Ref: " + hRef;
+        let res = code + " - " + name;
+        codes.push(code);
+        codeDesc.push(desc);
+        ticketRes.push(res);
+    } else if (type === "50"){
+        let code = initials + shortRef + "FTY" + number;
+        let desc = name + " £50 off £140 | Int Ref: " + intRef + " - HD Ref: " + hRef;
+        let res = code + " - " + name;
+        codes.push(code);
+        codeDesc.push(desc);
+        ticketRes.push(res);
+    }
+}
+
+function display(){
+    let cycles = codes.length;
+    for (let i = 0;i < cycles;i++){
+        $("#codes").append(codes[i] + "<br>");
+        $("#ref").append(codeDesc[i] + "<br>");
+        $("#res").append(ticketRes[i] + "<br>");
+    }
+}
